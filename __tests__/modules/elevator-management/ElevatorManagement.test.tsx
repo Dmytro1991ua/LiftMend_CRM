528abc05657581e6f3a1b6d0c158bd424a686550
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2024-04-05 09:17:33 +0300

    Cover SectionHeader functionality with tests (#31)

--- __tests__/modules/elevator-management/ElevatorManagement.test.tsx
+++ __tests__/modules/elevator-management/ElevatorManagement.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from '@testing-library/react';
 
 import ElevatorManagement from '@/modules/elevator-management';
+import { SectionHeaderTitle } from '@/types/enums';
 
 describe('ElevatorManagement', () => {
   it('should render component without crashing', () => {
     render(<ElevatorManagement />);
 
-    expect(screen.getByText('Section Header')).toBeInTheDocument();
+    expect(screen.getByText(SectionHeaderTitle.ElevatorManagement)).toBeInTheDocument();
     expect(screen.getByText('Elevator Management content')).toBeInTheDocument();
   });
 });
--- __tests__/modules/profile/Profile.test.tsx
+++ __tests__/modules/profile/Profile.test.tsx
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
 
 import Profile from '@/modules/profile';
+import { SectionHeaderTitle } from '@/types/enums';
 
 describe('Profile', () => {
   it('should render component without crashing', () => {
     render(<Profile />);
 
-    expect(screen.getByText('Profile')).toBeInTheDocument();
+    expect(screen.getByText(SectionHeaderTitle.Profile)).toBeInTheDocument();
+    expect(screen.getByText('Profile content')).toBeInTheDocument();
   });
 });
