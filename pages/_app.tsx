918cbe87a509af971b33635c6b01356f4607074c
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2024-04-23 16:49:49 +0300

    Intriduce Base Error Boundary component to properly handle app errors (#45)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,22 @@ import '@/styles/globals.css';
 
 import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { Toaster } from '@/components/ui/toaster';
 import { client } from '@/graphql';
 import MainLayout from '@/modules/layout/MainLayout';
+import BaseErrorBoundary from '@/shared/basseErroBoundary';
+import { logError } from '@/types/utils';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ApolloProvider client={client}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ErrorBoundary FallbackComponent={BaseErrorBoundary} onError={logError}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ErrorBoundary>
       <Toaster />
     </ApolloProvider>
   );
---
+++ shared/basseErroBoundary/BaseErrorBoundary.tsx
@@ -0,0 +1,22 @@
+import { ComponentType } from 'react';
+import { FallbackProps } from 'react-error-boundary';
+
+import { Button } from '@/components/ui/button';
+
+const BaseErrorBoundary: ComponentType<FallbackProps> = ({ error, resetErrorBoundary }) => {
+  return (
+    <section className='flex justify-center items-center h-screen bg-background '>
+      <div className='bg-white w-[95%] md:w-4/5 lg:w-3/5 xl:w-2/5 p-3 rounded-2xl text-center drop-shadow-lg'>
+        <h2 className='text-3xl font-bold mb-3'>Application Error</h2>
+        <h3 className='text-link'>The application has stopped responding unexpectedly</h3>
+        <h3 className='text-link mb-2'>
+          Try to refresh the page by clicking on a button below or contact application administrator
+        </h3>
+        <p className='text-red-800 mb-4'>{error?.message}</p>
+        <Button onClick={resetErrorBoundary}>Refresh</Button>
+      </div>
+    </section>
+  );
+};
+
+export default BaseErrorBoundary;
