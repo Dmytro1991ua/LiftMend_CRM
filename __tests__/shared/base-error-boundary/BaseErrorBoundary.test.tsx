8f9e59331bf7be8fa5f7779b2d90485b58c03db8
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2024-04-24 13:16:45 +0300

    Cover Base Error Boundary functionality with tests

---
+++ __tests__/shared/base-error-boundary/BaseErrorBoundary.test.tsx
@@ -0,0 +1,43 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useEffect } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import BaseErrorBoundary from '@/shared/base-error-boundary/BaseErrorBoundary';
+
+const ComponentWithError = (): JSX.Element => {
+  useEffect(() => {
+    throw new Error('Something went wrong');
+  }, []);
+  return <div />;
+};
+
+describe('<ErrorBoundary />', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should render error boundary when application error occurs', async () => {
+    const mockError = jest.fn();
+
+    render(
+      <MockedProvider addTypename={false} mocks={[]}>
+        <ErrorBoundary FallbackComponent={BaseErrorBoundary} onError={mockError}>
+          <ComponentWithError />
+        </ErrorBoundary>
+      </MockedProvider>
+    );
+
+    expect(screen.getByTestId('base-error-boundary')).toHaveTextContent('Something went wrong');
+    expect(screen.getByTestId('base-error-boundary')).toBeInTheDocument();
+
+    expect(mockError).toHaveBeenCalledTimes(1);
+
+    await userEvent.click(screen.getByTestId('reset-error-boundary-btn'));
+
+    await waitFor(() => {
+      expect(mockError).toHaveBeenCalledTimes(2);
+    });
+  });
+});
