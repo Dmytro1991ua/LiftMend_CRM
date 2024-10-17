19e1de7de4edd2fcf7755773677e833117032aff
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2024-10-17 22:18:51 +0300

    Adjust export file for elevator records table

--- shared/base-table/export-button/config.ts
+++ shared/base-table/export-button/config.ts
@@ -8,12 +8,18 @@ const accessors = {
   startDate: (cell: Cell<TableModel, TableValue>): string => formatDate(cell.row.original.startDate),
   endDate: (cell: Cell<TableModel, TableValue>): string => formatDate(cell.row.original.endDate),
   technicianSkills: (cell: Cell<TableModel, TableValue>): string => cell.row.original.technicianSkills.join(', '),
+  lastMaintenanceDate: (cell: Cell<TableModel, TableValue>): string =>
+    formatDate(cell.row.original.lastMaintenanceDate),
+  nextMaintenanceDate: (cell: Cell<TableModel, TableValue>): string =>
+    formatDate(cell.row.original.nextMaintenanceDate),
 };
 
-const { startDate, endDate, technicianSkills } = accessors;
+const { startDate, endDate, technicianSkills, lastMaintenanceDate, nextMaintenanceDate } = accessors;
 
 export const columnsValueAccessors: ColumnsValueAccessors = {
   startDate,
-  endDate: endDate,
+  endDate,
   technicianSkills,
+  lastMaintenanceDate,
+  nextMaintenanceDate,
 };
