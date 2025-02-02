dff253c8c95e3b7ff8da1d828056f7f33d96d14d
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2025-02-02 11:00:31 +0200

    Rework approach to merge GQL schemas to make it work in production

--- pages/api/graphql/schemas/index.ts
+++ pages/api/graphql/schemas/index.ts
@@ -1,17 +1,38 @@
+import fs from 'fs';
 import path from 'path';
 
-import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
 const SCHEMAS_FOLDER_PATH = 'pages/api/graphql/schemas';
-const FILE_EXTENSION = '**/*.graphql';
+const FILE_EXTENSION = /\.graphql$/;
+// Priority schema files to load first
+const PRIORITY_SCHEMAS = ['query.graphql', 'mutation.graphql'];
+
+// Reads schema file content if it exists
+const readSchemaFile = (filePath: string): string | null => {
+  if (fs.existsSync(filePath)) {
+    return fs.readFileSync(filePath, 'utf-8');
+  }
+
+  return null;
+};
 
 const generateMergedTypeDefs = () => {
-  const folderPath = path.join(process.cwd(), SCHEMAS_FOLDER_PATH);
+  // Combine priority schemas and other GraphQL files from the folder
+  const allSchemas = [...PRIORITY_SCHEMAS, ...fs.readdirSync(SCHEMAS_FOLDER_PATH)];
 
-  const schemaFiles = loadFilesSync(path.join(folderPath, FILE_EXTENSION));
+  const schemaFiles = allSchemas.reduce((acc, file) => {
+    // Only process files that match .graphql extension or are in PRIORITY_SCHEMAS
+    if (FILE_EXTENSION.test(file) || PRIORITY_SCHEMAS.includes(file)) {
+      const content = readSchemaFile(path.join(SCHEMAS_FOLDER_PATH, file));
 
-  return mergeTypeDefs(schemaFiles);
+      if (content) acc.add(content);
+    }
+
+    return acc;
+  }, new Set<string>());
+
+  return mergeTypeDefs(Array.from(schemaFiles));
 };
 
 export default generateMergedTypeDefs();
