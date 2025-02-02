1d992ad6d6342abc661a8235eea03d6dd0352637
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2025-02-02 22:40:21 +0200

    Debug schema merge util on prod

--- pages/api/graphql/schemas/index.ts
+++ pages/api/graphql/schemas/index.ts
@@ -3,6 +3,17 @@ import path from 'path';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
+import './schemas/query.graphql';
+import './schemas/mutation.graphql';
+import './schemas/calendarEvent.graphql';
+import './schemas/elevatorRecord.graphql';
+import './schemas/pagination.graphql';
+import './schemas/repairJob.graphql';
+import './schemas/scalar.graphql';
+import './schemas/technicianRecord.graphql';
+import './schemas/user.graphql';
+import './schemas/dashboard.graphql';
+
 const SCHEMAS_FOLDER_PATH = 'pages/api/graphql/schemas';
 const FILE_EXTENSION = '**/*.graphql';
 
