24824e160abdbc63270bfbc20b34e779226b105f
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2025-02-02 12:52:25 +0200

    Rework approach of schema merging in order to work in production

--- pages/api/graphql/schemas/index.ts
+++ pages/api/graphql/schemas/index.ts
@@ -1,41 +1,27 @@
-import fs from 'fs';
-import path from 'path';
-
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
-const SCHEMAS_FOLDER_PATH = path.join(process.cwd(), 'pages/api/graphql/schemas');
-const FILE_EXTENSION = /\.graphql$/;
-// Priority schema files to load first
-const PRIORITY_SCHEMAS = ['query.graphql', 'mutation.graphql'];
+import calendarEventSchema from './calendarEvent.graphql';
+import dashboardSchema from './dashboard.graphql';
+import elevatorRecordSchema from './elevatorRecord.graphql';
+import mutationSchema from './mutation.graphql';
+import paginationSchema from './pagination.graphql';
+import querySchema from './query.graphql';
+import repairJobSchema from './repairJob.graphql';
+import scalarSchema from './scalar.graphql';
+import technicianRecordSchema from './technicianRecord.graphql';
+import userSchema from './user.graphql';
 
-// Reads schema file content if it exists
-const readSchemaFile = (filePath: string): string | null => {
-  if (fs.existsSync(filePath)) {
-    return fs.readFileSync(filePath, 'utf-8');
-  }
+const schemas = [
+  calendarEventSchema,
+  elevatorRecordSchema,
+  paginationSchema,
+  querySchema,
+  repairJobSchema,
+  scalarSchema,
+  technicianRecordSchema,
+  userSchema,
+  mutationSchema,
+  dashboardSchema,
+];
 
-  return null;
-};
-
-const generateMergedTypeDefs = () => {
-  // Combine priority schemas and other GraphQL files from the folder
-  const allSchemas = [...PRIORITY_SCHEMAS, ...fs.readdirSync(SCHEMAS_FOLDER_PATH)];
-
-  const schemaFiles = allSchemas.reduce((acc, file) => {
-    // Only process files that match .graphql extension or are in PRIORITY_SCHEMAS
-    if (FILE_EXTENSION.test(file) || PRIORITY_SCHEMAS.includes(file)) {
-      const content = readSchemaFile(path.join(SCHEMAS_FOLDER_PATH, file));
-
-      if (content) acc.add(content);
-    }
-
-    console.log('Schema folder path:', SCHEMAS_FOLDER_PATH);
-    console.log('Files found:', fs.readdirSync(SCHEMAS_FOLDER_PATH));
-
-    return acc;
-  }, new Set<string>());
-
-  return mergeTypeDefs(Array.from(schemaFiles));
-};
-
-export default generateMergedTypeDefs();
+export const typeDefs = mergeTypeDefs(schemas);
