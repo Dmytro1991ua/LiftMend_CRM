d87482b079c9a74ef5cee56093e2d3154cb15baf
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2025-02-28 13:24:10 +0200

    Add Auth middlware (#290)

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -3,4 +3,10 @@ import { SupabaseClient, createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-export const supabaseClient = createClient<SupabaseClient>(supabaseUrl, supabaseAnonKey);
+export const supabaseClient = createClient<SupabaseClient>(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
---
+++ middleware.ts
@@ -0,0 +1,44 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextRequest, NextResponse } from 'next/server';
+
+import { AppRoutes } from './types/enums';
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  const supabase = createMiddlewareClient({ req, res });
+
+  await supabase.auth.getSession();
+
+  const reqUrl = req.nextUrl.pathname;
+
+  const token = req ? req.cookies?.get('sb-qgaqvfqcyjcewjnntzci-auth-token') : null;
+
+  if (reqUrl.startsWith('/_next')) return NextResponse.next();
+
+  if (token && reqUrl === AppRoutes.Default) {
+    const clonedUrl = req.nextUrl.clone();
+
+    clonedUrl.pathname = AppRoutes.Dashboard;
+
+    return NextResponse.redirect(clonedUrl);
+  }
+
+  if (!token && (reqUrl.startsWith(AppRoutes.SignIn) || reqUrl.startsWith(AppRoutes.SignUp))) {
+    return NextResponse.next();
+  }
+
+  if ((reqUrl.includes(AppRoutes.SignIn) || reqUrl.includes(AppRoutes.SignUp)) && token) {
+    return NextResponse.redirect(new URL(AppRoutes.Dashboard, req.url));
+  }
+
+  if (!token && !(reqUrl.includes(AppRoutes.SignIn) || reqUrl.includes(AppRoutes.SignUp))) {
+    return NextResponse.redirect(new URL(AppRoutes.SignIn, req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ['/', '/((?!api|_next/static|_next/image|favicon.ico|api/auth).*)'],
+};
