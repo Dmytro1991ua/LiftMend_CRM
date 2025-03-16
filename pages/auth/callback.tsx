e0470a06fffafba569053ca69100a6e71705bdff
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2025-03-16 16:29:02 +0200

    Introduce Oauth callback cutom page

---
+++ pages/auth/callback.tsx
@@ -0,0 +1,8 @@
+import OAuthCallback from '@/shared/auth/oauth-callback';
+import { NextPageWithLayout } from '@/shared/types';
+
+const OAuthCallbackPage: NextPageWithLayout = () => {
+  return <OAuthCallback />;
+};
+
+export default OAuthCallbackPage;
